429b3cd77572b0fed0e3f71d91cb079557d8d0d3
Nikita Shchypylov <sn97@icloud.com>
2018-05-03 15:08:48 +0200

    init 5

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+import "regenerator-runtime/runtime";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./containers/App/index"
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from "redux"
+import reducers from './reducers';
+
+import 'reset-css/reset.css';
+
+const reduxDevTools =
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(
+  reducers,
+  reduxDevTools,
+  applyMiddleware()
+)
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+   document.getElementById("root")
+ );
