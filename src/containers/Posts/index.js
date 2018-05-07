891d4512a62c822a764a51ec85737324f5d820df
Nikita Shchypylov <sn97@icloud.com>
2018-05-07 17:57:33 +0200

    updated flow, new config

--- src/containers/Posts/index.js
+++ src/containers/Posts/index.js
@@ -1,59 +1,62 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import _ from "lodash"
+// @flow
 
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
 
-class Posts extends Component {
-	
+type Props = {
+	posts: {
+		mode: string,
+		items: Array<{
+			data: Array<{ nasa_id: string, description: string }>,
+			links: Array<{ href: string }>
+		}>
+	}
+};
+
+class Posts extends Component<Props> {
 	renderPosts = () => {
-		const {posts} = this.props;
-		let {mode, items} = posts;
-		console.log("--- items", items)
-		return !(_.isEmpty(items)) ? items.map((value, key) => {
-			console.log("--- posts.mode", mode)
-			if ( mode === "image" && value.data[0].media_type === "image") {
-				return (
-					<div className="post post--image" key={value.data[0].nasa_id}>
-						<div className="post-title">
-							{value.data[0].description}
+		const { posts } = this.props;
+		console.log(posts);
+		let { mode, items } = posts;
+		console.log(items);
+		return !_.isEmpty(items) ? (
+			items.map((value, key) => {
+				console.log(typeof value);
+				if (mode === "image" && value.data[0].media_type === "image") {
+					return (
+						<div className="post post--image" key={value.data[0].nasa_id}>
+							<div className="post-title">{value.data[0].description}</div>
+							<div className="post-image">
+								<img src={value.links[0].href} alt="" />
+							</div>
 						</div>
-						<div className="post-image">
-							<img src={value.links[0].href} alt=""/>
+					);
+				} else if (mode === "video" && value.data[0].media_type === "video") {
+					return (
+						<div className="post post--video" key={value.data[0].nasa_id}>
+							<div className="post-title">{value.data[0].description}</div>
+							<div className="post-image">
+								<img src={value.links[0].href} alt="" />
+							</div>
 						</div>
-					</div>
-				)
-			} else if (mode === "video" && value.data[0].media_type === "video" ) {
-				return (
-					<div className="post post--video" key={value.data[0].nasa_id}>
-						<div className="post-title">
-							{value.data[0].description}
-						</div>
-						<div className="post-image">
-							<img src={value.links[0].href} alt=""/>
-						</div>
-					</div>
-				)
-			}
-		}) : (
+					);
+				}
+			})
+		) : (
 			<div>No posts</div>
-		)
-	};
-	
-	render() {
-		const {posts} = this.props;
-		return (
-			<div>
-				{this.renderPosts()}
-			</div>
 		);
+	};
+
+	render() {
+		const { posts } = this.props;
+		return <div>{this.renderPosts()}</div>;
 	}
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
 	return {
 		posts: state.posts
-	}
+	};
 };
-export default connect(
-	mapStateToProps,
-)(Posts);
+export default connect(mapStateToProps)(Posts);
--- src/presentational/SearchForm.js
+++ src/presentational/SearchForm.js
@@ -1,26 +1,44 @@
 //@flow
 
-import React from "react"
+import React from "react";
 
 type Props = {
-	title : string,
-	inputHandle : (value:string) => mixed,
-	radioChangeHandle : (value:string) => mixed,
-	submitHandle : (value:string) => mixed,
-}
+	title: string,
+	inputHandle: (value: SyntheticInputEvent<>) => mixed,
+	radioChangeHandle: (value: SyntheticInputEvent<>) => mixed,
+	submitHandle: (value: SyntheticEvent<>) => mixed
+};
 
-const SearchForm = ({title, inputHandle, radioChangeHandle, submitHandle}: Props) => {
+const SearchForm = ({
+	title,
+	inputHandle,
+	radioChangeHandle,
+	submitHandle
+}: Props) => {
 	return (
 		<div>
 			<h1>{title}</h1>
 			<form action="#" id="search-form">
-				<input type="text" id="searchInput" onChange={inputHandle}/>
-				<input type="radio" name="radio-button" value="image" defaultChecked={true} onChange={radioChangeHandle}/> Image
-				<input type="radio" name="radio-button" value="video" onChange={radioChangeHandle}/> Video
-				<input type="submit" value="Go" onClick={submitHandle}/>
+				<input type="text" id="searchInput" onChange={inputHandle} />
+				<input
+					type="radio"
+					name="radio-button"
+					value="image"
+					defaultChecked={true}
+					onChange={radioChangeHandle}
+				/>{" "}
+				Image
+				<input
+					type="radio"
+					name="radio-button"
+					value="video"
+					onChange={radioChangeHandle}
+				/>{" "}
+				Video
+				<input type="submit" value="Go" onClick={submitHandle} />
 			</form>
 		</div>
-	)
-}
+	);
+};
 
-export default SearchForm
+export default SearchForm;
